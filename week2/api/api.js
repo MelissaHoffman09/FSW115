0dc6f08a5d04a90085f15a62d30cf149dc0e27b5
MelissaHoffman09 <melissa.hoffman@bryanuniversity.edu>
2021-02-28 22:38:11 -0800

    3

--- week2/api/api.js
+++ week2/api/api.js
@@ -1,29 +1,37 @@
-const starWars = {
-        "name": "Luke Skywalker",
-        "height": "172",
-        "mass": "77",
-        "hair_color": "blond",
-        "skin_color": "fair",
-        "eye_color": "blue",
-        "birth_year": "19BBY",
-        "gender": "male",
-        "homeworld": "http://swapi.dev/api/planets/1/",
-        "films": [
-            "http://swapi.dev/api/films/1/",
-            "http://swapi.dev/api/films/2/",
-            "http://swapi.dev/api/films/3/",
-            "http://swapi.dev/api/films/6/"
-        ],
-        "species": [],
-        "vehicles": [
-            "http://swapi.dev/api/vehicles/14/",
-            "http://swapi.dev/api/vehicles/30/"
-        ],
-        "starships": [
-            "http://swapi.dev/api/starships/12/",
-            "http://swapi.dev/api/starships/22/"
-        ],
-        "created": "2014-12-09T13:50:51.644000Z",
-        "edited": "2014-12-20T21:17:56.891000Z",
-        "url": "http://swapi.dev/api/people/1/"
-    }
+var starWars = {
+    "name": "Luke Skywalker",
+    "height": "172",
+    "mass": "77",
+    "hair_color": "blond",
+    "skin_color": "fair",
+    "eye_color": "blue",
+    "birth_year": "19BBY",
+    "gender": "male",
+    "homeworld": "http://swapi.dev/api/planets/1/",
+    "films": [
+        "http://swapi.dev/api/films/1/",
+        "http://swapi.dev/api/films/2/",
+        "http://swapi.dev/api/films/3/",
+        "http://swapi.dev/api/films/6/"
+    ],
+    "species": [],
+    "vehicles": [
+        "http://swapi.dev/api/vehicles/14/",
+        "http://swapi.dev/api/vehicles/30/"
+    ],
+    "starships": [
+        "http://swapi.dev/api/starships/12/",
+        "http://swapi.dev/api/starships/22/"
+    ],
+    "created": "2014-12-09T13:50:51.644000Z",
+    "edited": "2014-12-20T21:17:56.891000Z",
+    "url": "http://swapi.dev/api/people/1/"
+}
+
+const values = Object.values(starWars);
+const entries = Object.entries(starWars);
+const data = document.getElementById('data');
+
+data.innerHTML = '<ul>' + entries.map(function(item) {  
+    return `<li> ${item[0]}${item[1]} </li>`;         
+}).join('') + '</ul>'
