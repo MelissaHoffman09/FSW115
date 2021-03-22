a88d061a8cda24f97065fd6e79c79a9ca3f4cfbc
Melissa Hoffman <73321224+MelissaHoffman09@users.noreply.github.com>
2021-03-22 00:26:47 -0700

    2

--- week5/app.js
+++ week5/app.js
@@ -1,107 +1,102 @@
 // Get 
 const getTodo = () => {
     axios
-      .get("https://api.vschool.io/MelissaH/todo/")
-      .then((res) => {
+        .get("https://api.vschool.io/MelissaH/todo/")
+        .then((res) => {
         displayItems(res.data);
-      })
-      .catch((error) => console.error(error));
+    })
+        .catch((error) => console.error(error));
   };
-  getTodo();
+    getTodo();
   
-  const displayItems = (data) => {
+const displayItems = (data) => {
     console.log(data);
     let box = document.getElementById("toDoItems");
   
-    data.forEach((todo) => {
-      let todoBox = document.createElement("div");
-      let title = document.createElement("h2");
-      let description = document.createElement("h4");
-      let price = document.createElement("h4");
-      todoBox.className = "todoContainer"
-      title.textContent = "Title: " + todo.title;
-      description.textContent = "Description: " + todo.description;
-      price.textContent = "$" + todo.price;
+data.forEach((todo) => {
+    let todoBox = document.createElement("div");
+    let title = document.createElement("h2");
+    let description = document.createElement("h4");
+    let price = document.createElement("h4");
+        todoBox.className = "todoContainer"
+        title.textContent = "Title: " + todo.title;
+        description.textContent = "Description: " + todo.description;
+        price.textContent = "$" + todo.price;
   
-      let deleteButton = document.createElement("button");
-      deleteButton.innerText = "Delete";
-      deleteButton.className = "button2"
-      deleteButton.id = todo._id;
-      deleteButton.addEventListener("click", deleteItem);
-  
-      let completeButton = document.createElement("button");
-      let completeButtonText = "";
-      completeButton.className = "button3"
-      todo.completed
-        ? (completeButtonText = "Incomplete" )
-        : (completeButtonText = "Completed");
-  
-      completeButton.innerText = completeButtonText;
-      completeButton.id = todo._id;
-      completeButton.value = todo.completed;
-      completeButton.addEventListener("click", updateItem);
-  
-      todoBox.appendChild(title);
-      todoBox.appendChild(description);
-      todoBox.appendChild(price);
-      box.appendChild(todoBox);
-      todoBox.appendChild(completeButton);
-      todoBox.appendChild(deleteButton);
+    let deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.className = "button2"
+        deleteButton.id = todo._id;
+        deleteButton.addEventListener("click", deleteItem);
+    
+    let completeButton = document.createElement("button");
+    let completeButtonText = "";
+        completeButton.className = "button3"
+        todo.completed
+            ? (completeButtonText = "Incomplete" )
+            : (completeButtonText = "Completed" );
+        completeButton.innerText = completeButtonText;
+        completeButton.id = todo._id;
+        completeButton.addEventListener("click", updateItem);
+    
+        todoBox.appendChild(title);
+        todoBox.appendChild(description);
+        todoBox.appendChild(price);
+        box.appendChild(todoBox);
+        todoBox.appendChild(completeButton);
+        todoBox.appendChild(deleteButton);
     });
   };
   
   // Post
-  const postTodo = (event) => {
-    event.preventDefault();
-    console.log("");
-  
+    const postItem = (event) => {
+        event.preventDefault();
+        console.log("");
+    
     const itemTitle = document.getElementById("input1");
     const itemDescription = document.getElementById("input2");
     const itemPrice = document.getElementById("input3");
     const itemComplete = document.getElementById("inputComplete");
-    console.dir(itemComplete);
+        console.dir(itemComplete);
   
     let newItem = {
-      title: itemTitle.value,
-      description: itemDescription.value,
-      price: itemPrice.value,
-      completed: itemComplete.checked,
+        title: itemTitle.value,
+        description: itemDescription.value,
+        price: itemPrice.value,
+        completed: itemComplete.checked,
     };
-    console.log(newItem);
+        console.log(newItem);
     axios
-      .post("https://api.vschool.io/MelissaH/todo/", newItem)
-      .then(() => {
-        location.reload();
+        .post("https://api.vschool.io/MelissaH/todo/", newItem)
+        .then(() => {
+            location.reload();
       })
-      .catch((err) => console.log(err));
+        .catch((err) => console.log(err));
   };
-  const myTodo = document.getElementById("myTodo");
-  myTodo.addEventListener("submit", postTodo);
+  const myForm = document.getElementById("myForm");
+  myForm.addEventListener("submit", postItem);
   
   // Delete
-  const deleteItem = (event) => {
+    const deleteItem = (event) => {
     console.log(event.target.id);
     axios
-      .delete(`https://api.vschool.io/MelissaH/todo/${event.target.id}`)
-      .then(() => {
-        location.reload();
-      })
-      .catch((err) => console.log(err));
+        .delete(`https://api.vschool.io/MelissaH/todo/${event.target.id}`)
+        .then(() => {
+            location.reload();
+    })
+        .catch((err) => console.log(err));
   };
   
   // Put
-  const updateItem = (event) => {
-    console.log(event.target.value);
-    isCompleted = null;
-    event.target.value === "true" ? (isCompleted = true) : (isCompleted = false);
+    const updateItem = (event) => {
+        console.log(event.target.value);
+        isCompleted = null;
+        event.target.value === "true" ? (isCompleted = true) : (isCompleted = false);
     let updatedItem = { completed: !isCompleted };
     
   
     axios
-      .put(
-        `https://api.vschool.io/MelissaH/todo/${event.target.id}`,
-        updatedItem
-      )
-      .then(() => location.reload())
-      .catch((err) => console.log(err));
+        .put(`https://api.vschool.io/MelissaH/todo/${event.target.id}`, updatedItem)
+        .then(() => location.reload())
+        .catch((err) => console.log(err));
   };
